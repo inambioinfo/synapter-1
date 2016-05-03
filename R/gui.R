d9b588f48acd49ab7efe26b2c74f099b9763fdba
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-05-03 21:26:31 +0000

    Creating branch for BioC 3.3 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_3/madman/Rpacks/synapter@117080 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/gui.R
@@ -0,0 +1,5 @@
+synapterGUI <- function(n = 1) {
+    .Defunct("synergise", package = "synapter",
+             msg = c("The synapterGUI() interface is defunct.\n",
+                     "Please use synergise() instead."))
+}
